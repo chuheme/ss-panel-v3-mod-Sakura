20a0b087ef59ca803d048eca40a0a9f37cf40575
SakuraSa233 <sakura.sa233@gmail.com>
2019-04-14 23:33:24 +0800

    feat: theme and json output

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -12,9 +12,26 @@ class BaseController
 {
     public $user;
     
+    /**
+     * Get user and define TEMPLATE_PATH
+     */
     public function __construct()
     {
-        $this->user =  Auth::getUser();
+        $user = Auth::getUser();
+        $this->user = $user;
+
+        if ($user->isLogin) {
+            define('TEMPLATE_PATH', BASE_PATH . '/resources/views/' . $user->theme . '/');
+        } else {
+            define('TEMPLATE_PATH', BASE_PATH . '/resources/views/' . $_ENV['theme'] . '/');
+        }
     }
 
+    /**
+     * Response as encoded json
+     */
+    public function echoJson($response, $res)
+    {
+        return $response->getBody()->write(json_encode($res));
+    }
 }
