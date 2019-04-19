529fb6b7e81ebb7128e19c67412a46a8fb757fb9
SakuraSa233 <sakura.sa233@gmail.com>
2019-04-19 15:47:44 +0800

    feat: uncontained renderer

--- app/Services/View.php
+++ app/Services/View.php
@@ -2,29 +2,25 @@
 
 namespace App\Services;
 
-use Smarty;
-
 class View
 {
-    public static function getSmarty()
+    /**
+     * generate a renderer
+     * 
+     * @return callable
+     */
+    public static function renderer()
     {
-        $smarty=new smarty(); //实例化smarty
-        
         $user = Auth::getUser();
-        
+
         if ($user->isLogin) {
-            $theme=$user->theme;
+            $theme = $user->theme;
         } else {
-            $theme=Config::get('theme');
+            $theme = Config::get('theme');
         }
         
-        
-        $smarty->settemplatedir(BASE_PATH.'/resources/views/'.$theme.'/'); //设置模板文件存放目录
-        $smarty->setcompiledir(BASE_PATH.'/storage/framework/smarty/compile/'); //设置生成文件存放目录
-        $smarty->setcachedir(BASE_PATH.'/storage/framework/smarty/cache/'); //设置缓存文件存放目录
-        // add config
-        $smarty->assign('config', Config::getPublicConfig());
-        $smarty->assign('user', Auth::getUser());
-        return $smarty;
+        $renderer = new \Slim\Views\PhpRenderer(BASE_PATH . '/resources/views/' . $theme . '/');
+        $renderer->addAttribute('user', $user);
+        return $renderer;
     }
 }
